457a80721ffb96dc69201d6453001f4fed26d947
HunbalSiddiqui <hunbal.dha@gmail.com>
2023-05-16 18:06:19 +0500

    app06 init.

---
+++ currencyConverter06/src/App.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import type { PropsWithChildren } from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+function App(): JSX.Element {
+
+
+  return (
+    <View>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
---
+++ currencyConverter06/src/components/CurrencyButton.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CurrencyButton() {
+    return (
+        <div>CurrencyButton</div>
+    )
+}
+
+export default CurrencyButton
